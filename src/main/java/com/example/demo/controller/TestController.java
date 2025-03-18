84ae69d9303c459fd24048aa0f84daac45140915
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-03-18 11:42:14 +0900

    response body get 요청 보내기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController //웹 요청 처리
 @RequestMapping("test") //url 지정
@@ -30,5 +34,18 @@ public class TestController {
     @GetMapping("/testRequestBody")
     public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO){
         return "Hello World" + testRequestBodyDTO.getId() + " Message: " + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody(){
+        //응답 data에 들어갈 list 생성
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseDTO");
+        list.add("Kim Jin Seon");
+        //ResponseDTO 생성
+        ResponseDTO<String> responseDTO = ResponseDTO.<String>builder() //빌더 생성
+                                                        .data(list) //데이터 입력
+                                                        .build(); //빌드
+        return responseDTO;
     }
 }
