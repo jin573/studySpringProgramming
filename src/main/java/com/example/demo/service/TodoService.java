55ccd59becdb901a8bf86966faf959c983b62237
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-03-20 10:01:34 +0900

    Service 에서 Repository에 저장하고 검색하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,10 +1,23 @@
 package com.example.demo.service;
 
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+
+    @Autowired
+    private TodoRepository repository;
+
     public String testService(){
-        return "Test Service";
+        //TodoEntity 생성
+        TodoEntity todoEntity = TodoEntity.builder().title("My first todo item").build();
+        //TodoEntity 저장
+        repository.save(todoEntity);
+        //TodoEntity 검색
+        TodoEntity savedEntity = repository.findById(todoEntity.getId()).get();
+        return savedEntity.getTitle();
     }
 }
