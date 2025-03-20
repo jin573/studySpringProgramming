6c4cd1d6c7ed5aab863aaee41445932c042e6fbf
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-03-20 09:49:19 +0900

    TodoService 자동 주입하고 TodoEntity를 JPA 사용하여 연결하기 (테이블 생성)

---
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("todo") //localhost:8080/todo
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo(){
+        String str = todoService.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+
+        ResponseDTO<String> responseEntity = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(responseEntity);
+    }
+}
--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,16 +1,26 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name="Todo")
 public class TodoEntity {
-    private String id;
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String id;//primary key니까 자동으로 생성되게 -> GeneratedValue, GenericGenerator
     private String userId;
     private String title;
     private boolean done;
