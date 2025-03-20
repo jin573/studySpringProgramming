a81a16c4f7dee2452c27954c201789f13095490c
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-03-20 10:22:53 +0900

    TodoRepository에 새로운 메소드 추가

--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -2,9 +2,17 @@ package com.example.demo.persistence;
 
 import com.example.demo.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    //save, findById, delete 는 자동으로 구현된다
 
+    //새로운 메소드 추가할 수 있음. findByUserId 등
+    @Query(value = "select * from Todo t where t.userid = ?1")
+    List<TodoEntity> findByUserId(String userId);
+    List<TodoEntity> findByUserIdAndTitle(String userId, String title);
 }
