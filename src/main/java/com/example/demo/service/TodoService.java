91d1b5ef468ba43c0c714ae4bc270b6df9b51e7f
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-03-25 10:42:14 +0900

    Slf4j 로깅 등록

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,9 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class TodoService {
 
